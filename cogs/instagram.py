d51563a338e8ad6d333d29e2efb105621e5e8f73
astamin <134150651+astamin@users.noreply.github.com>
2024-01-07 15:44:52 +0100

    Update instagram.py

--- cogs/instagram.py
+++ cogs/instagram.py
@@ -87,17 +87,16 @@ class Instagram(commands.Cog):
     @commands.slash_command(name='sethelp' , description="Information about settings")
     @commands.is_owner()
     async def information(self , ctx):
-        await ctx.response.send_message("Please Send info as this in this command **setsetting**\n```target:username:password:email:bio:name```")
+        await ctx.response.send_message(content="```Note 📵 \n - I'm not the responsable of anything \n - If u get anything send it here [telegram](t.me/telllonym) \n - Bot is in devellopement status \n - Owner : ASTA 404 ```")
     @commands.slash_command(name="setsetting" , description="Set setting of auto")
     @commands.is_owner()
-    async def setsetting(self,interaction:discord.Interaction , j) :
-        info = j.split(':')
-        self.target = info[0]
-        self.username = info[1]
-        self.password=info[2]
-        self.email=info[3]
-        self.bio=info[4]
-        self.name=info[5]
+    async def setsetting(self,interaction:discord.Interaction , traget , user , password , email , bio , name) :
+        self.target = target
+        self.username = user
+        self.password=password
+        self.email=email
+        self.bio=bio
+        self.name=name
         await interaction.response.send_message(content="`Settings are saved`" , ephemeral=True)
     @commands.slash_command(name="deletesettings" , description="delete all settings")
     @commands.is_owner()
@@ -114,7 +113,7 @@ class Instagram(commands.Cog):
     @commands.is_owner()
     async def setting(self,interaction:discord.Interaction) :
         if self.name is None :
-            return await interaction.response.send_message(content="You don't have any saved" , ephemeral=True)
+            return await interaction.response.send_message(content="`You don't have any setting saved`" , ephemeral=True)
         user = self.username
         password = self.password
         email = self.email
