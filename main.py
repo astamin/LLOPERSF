b0b7226e830f4420549b209b362b0b5deff969fa
astamin <134150651+astamin@users.noreply.github.com>
2024-01-09 18:03:02 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,39 +1,16 @@
-try : 
-    import discord
-    import os
-    import asyncio
-    from discord.ext import commands
-    from dotenv import load_dotenv
-except :
-    import os
-    os.system('pip install -r requirements.txt')
-    import discord
-    import os
-    import asyncio
-    from discord.ext import commands
-    from dotenv import load_dotenv
-load_dotenv()
-TOKEN = input('Enter Token : ')
-class MyHelpCommand(commands.DefaultHelpCommand):
-    async def send_bot_help(self, mapping):
-        pass
-bot = commands.Bot(command_prefix='!', intents=discord.Intents.all() , help_command=MyHelpCommand())
-async def load():
-    for filename in os.listdir('./cogs'):
-        if filename.endswith('.py') :
-            cog_name = filename[:-3]
-            try:
-                await bot.load_extension(f'cogs.{cog_name}')
-                print(f'Loaded {cog_name} cog')
-            except Exception as e:
-                print(f'Failed to load {cog_name} cog. Error: {e}')
+import discord
+from discord.ext import commands 
+from discord.commands import application_command
+import requests , httpx , uuid
+import os
+token =input('Enter Token : ')
+from datetime import datetime
 
-@bot.event
-async def on_ready():
-    print("BOT IS ONLINE NOW {}".format(bot.user.display_name))
-def main():
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(load())
-    loop.run_until_complete(bot.run(TOKEN))
+bot = commands.Bot(command_prefix='/')
 
-main()
+@bot.application_command(name="start" , description="Start program")
+async def start(ctx:discord.Interaction):
+    await ctx.response.send_message("BOT IS STARTING AZBI !")
+
+bot.run(token=token)
+ 
